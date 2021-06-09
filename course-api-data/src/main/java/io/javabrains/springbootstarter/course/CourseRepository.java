79e66040e3b66aebd2950dc78e71c1dc6b85ac4e
Sreelekshmi R <Sreelekshmi R@DESKTOP-BTAA8IA>
2021-06-09 14:05:44 +0530

    First Commit

---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,16 @@
+package io.javabrains.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+
+	//public List<Course> findByTopicId(String topicId);
+
+	public Iterable<Course> findByTopicId(String topicId);
+
+	//public Iterable<Course> findByTopicId(String topicId);
+	
+	
+}
